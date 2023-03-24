1ec4da5dcede7949131551b3ad3a362b406fcc36
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-03-24 10:46:44 +0500

    INFRA: Create Build Project (CI)

--- LibraryManagment.Infrastructure.Build/Program.cs
+++ LibraryManagment.Infrastructure.Build/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace CashOverflow.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "Build & Test LibraryManagment.Web",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "master" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "master" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.WindowsLatest,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Checking out"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name= "Installing .NET",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "7.0.200"
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restoring packages"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Building project"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Running tests"
+                            }
+                        }
+                    }
+                }
+            };
+
+
+            var adotnetClient = new ADotNetClient();
+
+            adotnetClient.SerializeAndWriteToFile(
+                githubPipeline,
+                path: "../.github/workflows/build.yml");
+        }
+    }
+}
