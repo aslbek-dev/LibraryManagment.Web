36bbd5bffbb27da4f17eb00b4b38ebd4f77c34c3
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-04-04 19:04:16 +0500

    CODE RUB:  Clean up

--- LibraryManagment.Api/Services/Foundations/Rents/IRentService.cs
+++ LibraryManagment.Api/Services/Foundations/Rents/IRentService.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using LibraryManagment.Api.Models.Rents;
 
 namespace LibraryManagment.Api.Services.Foundations.Rents
