f3a2cb3a9bf76746fc960c5b30034caac9821d94
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-04-07 22:36:45 +0500

    ShouldRetrieveBookByIdAsync -> FAIL

--- LibraryManagment.Tests.Unit/Services/Foundations/Books/BookServiceTests.Logic.RetrieveById.cs
+++ LibraryManagment.Tests.Unit/Services/Foundations/Books/BookServiceTests.Logic.RetrieveById.cs
@@ -1,12 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LibraryManagment.Api.Models.Books;
+using Moq;
+using Xunit;
 
 namespace LibraryManagment.Tests.Unit.Services.Foundations.Books
 {
     public partial class BookServiceTests
     {
-        
+        [Fact]
+        public async Task ShouldRetrieveBookByIdAsync()
+        {
+            //give
+            Guid randomId = Guid.NewGuid();
+            Guid inputId = randomId;
+            Book randomBook = CreateRandomBook();
+            Book storageBook = randomBook;
+            Book expectedBook = storageBook.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectBookByIdAsync(randomId)).ReturnsAsync(storageBook);
+
+            //when
+            Book actualBook = 
+                await this.bookService.RetrieveBookByIdAsync(inputId);
+
+            //then
+            actualBook.Should().BeEquivalentTo(expectedBook);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectBookByIdAsync(inputId), Times.Once);
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
