981f5f5774c71dfd54e38144a5784b74d8d76a1a
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-04-07 22:48:26 +0500

    ShouldRetrieveUserByIdAsync -> FAIL

--- LibraryManagment.Tests.Unit/Services/Foundations/Users/UserServiceTests.Logic.RetrieveById.cs
+++ LibraryManagment.Tests.Unit/Services/Foundations/Users/UserServiceTests.Logic.RetrieveById.cs
@@ -1,12 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LibraryManagment.Api.Models.Users;
+using Moq;
+using Xunit;
 
 namespace LibraryManagment.Tests.Unit.Services.Foundations.Users
 {
     public partial class UserServiceTests
     {
-        
+        [Fact]
+        public async Task ShouldRetrieveUserByIdAsync()
+        {
+            //give
+            Guid randomId = Guid.NewGuid();
+            Guid inputId = randomId;
+            User randomUser = CreateRandomUser();
+            User storageUser = randomUser;
+            User expectedUser = storageUser.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(randomId)).ReturnsAsync(storageUser);
+
+            //when
+            User actualUser = 
+                await this.userService.RetrieveUserByIdAsync(inputId);
+
+            //then
+            actualUser.Should().BeEquivalentTo(expectedUser);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(inputId), Times.Once);
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
