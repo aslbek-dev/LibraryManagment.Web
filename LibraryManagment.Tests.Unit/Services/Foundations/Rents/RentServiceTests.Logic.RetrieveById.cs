faf778ba532ad771afccf81672c94a1a627443b9
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-04-07 22:42:12 +0500

    ShouldRetrieveRentByIdAsync -> FAIL

--- LibraryManagment.Tests.Unit/Services/Foundations/Rents/RentServiceTests.Logic.RetrieveById.cs
+++ LibraryManagment.Tests.Unit/Services/Foundations/Rents/RentServiceTests.Logic.RetrieveById.cs
@@ -1,12 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LibraryManagment.Api.Models.Rents;
+using Moq;
+using Xunit;
 
 namespace LibraryManagment.Tests.Unit.Services.Foundations.Rents
 {
     public partial class RentServiceTests
     {
-        
+        [Fact]
+        public async Task ShouldRetrieveRentByIdAsync()
+        {
+            //give
+            Guid randomId = Guid.NewGuid();
+            Guid inputId = randomId;
+            Rent randomRent = CreateRandomRent();
+            Rent storageRent = randomRent;
+            Rent expectedRent = storageRent.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectRentByIdAsync(randomId)).ReturnsAsync(storageRent);
+
+            //when
+            Rent actualRent = 
+                await this.rentService.RetrieveRentByIdAsync(inputId);
+
+            //then
+            actualRent.Should().BeEquivalentTo(expectedRent);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectRentByIdAsync(inputId), Times.Once);
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
