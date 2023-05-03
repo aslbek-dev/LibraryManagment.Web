273a03e2aa70a995f55cd845cb44e44de851bb22
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-05-03 11:52:57 +0500

    ShouldRemoveRentById -> FAIL

--- LibraryManagment.Tests.Unit/Services/Foundations/Rents/RentServiceTests.Logic.Remove.cs
+++ LibraryManagment.Tests.Unit/Services/Foundations/Rents/RentServiceTests.Logic.Remove.cs
@@ -2,11 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using LibraryManagment.Api.Models.Rents;
+using Moq;
+using Xunit;
 
 namespace LibraryManagment.Tests.Unit.Services.Foundations.Rents
 {
     public partial class RentServiceTests
     {
+        [Fact]
+        public async Task ShouldRemoveRentById()
+        {
+            // Given
+            Guid randomId = new Guid();
+            Guid inputId = randomId;
+            Rent randomRent = CreateRandomRent();
+            Rent storageRent = randomRent;
+            Rent expectedInputRent = storageRent;
+            Rent deletedRent = expectedInputRent;
+            Rent expectedRent = deletedRent.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectRentByIdAsync(inputId))
+                    .ReturnsAsync(storageRent);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteRentAsync(expectedInputRent))
+                    .ReturnsAsync(deletedRent);
+
+            // When
+            Rent actualRent = await this.rentService.RemoveRentByIdAsync(inputId);
         
+            // Then
+            actualRent.Should().BeEquivalentTo(expectedRent);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectRentByIdAsync(inputId), Times.Once);
+            
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteRentAsync(expectedInputRent), Times.Once);
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
