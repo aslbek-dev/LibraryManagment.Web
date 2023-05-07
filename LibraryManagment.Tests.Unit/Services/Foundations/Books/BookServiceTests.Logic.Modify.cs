0ed89cab802fcae0e3a3578cd51a42d3c3ff91c6
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-05-07 16:59:53 +0500

    Some changing

--- LibraryManagment.Tests.Unit/Services/Foundations/Books/BookServiceTests.Logic.Modify.cs
+++ LibraryManagment.Tests.Unit/Services/Foundations/Books/BookServiceTests.Logic.Modify.cs
@@ -7,6 +7,7 @@ namespace LibraryManagment.Tests.Unit.Services.Foundations.Books
 {
     public partial class BookServiceTests
     {
-        
+        [Fact]
+        public async Task ShouldModifyUser()
     }
 }
