b06f7ad0b70695e2c755b1e7f674f210f094d375
Aslbek Utkirovich <abdullayevaslbek069@gmail.com>
2023-05-15 23:27:29 +0500

    ShouldModifyBasket -> FAIL

--- LibraryManagment.Tests.Unit/Services/Foundations/Baskets/BasketServiceTests.Logic.Modify.cs
+++ LibraryManagment.Tests.Unit/Services/Foundations/Baskets/BasketServiceTests.Logic.Modify.cs
@@ -8,6 +8,29 @@ namespace LibraryManagment.Tests.Unit.Services.Foundations.Baskets
 {
     public partial class BasketServiceTests
     {
-    
+        [Fact]
+        public async Task ShouldModifyBasket()
+        {
+            //given
+            Basket randomBasket = CreateRandomBasket();
+            Basket inputBasket = randomBasket;
+            Basket modifiedBasket = inputBasket;
+            Basket expectedBasket = modifiedBasket.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateBasketAsync(inputBasket)).ReturnsAsync(modifiedBasket);
+            
+            // when
+            Basket actualBasket = await this.basketService.ModifyBasketAsync(inputBasket);
+
+            // then
+            actualBasket.Should().BeEquivalentTo(expectedBasket);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateBasketAsync(inputBasket), Times.Once);
+            
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
